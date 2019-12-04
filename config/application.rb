c93f055c3adecddc4bf47996081b9dcfb6b2521b
yu shimamura <mashiyu0914@gmail.com>
2019-12-04 10:59:50 +0900

    add: デバック用のgemの実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module Manyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
