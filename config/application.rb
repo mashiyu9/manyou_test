364274f84a4e4ceba946168256413e1ac7d44e6c
yu shimamura <mashiyu0914@gmail.com>
2019-12-04 12:16:30 +0900

    change: default localeをjaに
    initializers/locale.rbを作成

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Manyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.helper false
     end
