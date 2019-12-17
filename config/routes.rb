42f015eeeb85d15ae3a0b14fcf1b4b3a4652c996
yu shimamura <mashiyu0914@gmail.com>
2019-12-17 10:43:32 +0900

    add: user登録機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :tasks, only: [:index, :new, :create, :edit, :update, :show, :destroy]
 end
